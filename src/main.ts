13dfde02966b12fec27d71c242e6d2cbf7532c7f
HelmutM87 <127511280+HelmutM87@users.noreply.github.com>
2025-05-20 17:19:36 -0300

    changes finished

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import * as AOS from 'aos';
+import AOS from 'aos';
 import 'aos/dist/aos.css';
 
 AOS.init({
