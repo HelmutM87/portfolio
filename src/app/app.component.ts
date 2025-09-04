6e19be04fde8e3356342797954070ce5232f7b41
HelmutM87 <127511280+HelmutM87@users.noreply.github.com>
2025-09-04 13:27:45 +0200

    pictures deleted and fixes in components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterModule, RouterOutlet, Router, NavigationEnd } from '@angular/router';
 import { filter } from 'rxjs/operators';
-import { LandingPageComponent } from './landing-page/landing-page.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterModule, RouterOutlet, FooterComponent, LandingPageComponent, NavbarComponent],
+  imports: [RouterModule, RouterOutlet, FooterComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
